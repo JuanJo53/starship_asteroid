216edcff0e40ac63bea51de35d1640d8d86bf6f0
Juan José Fernández <juanjofer1999@gmail.com>
2020-06-14 19:19:03 -0400

    Antes de importar shared preferences

--- lib/jugador.dart
+++ lib/jugador.dart
@@ -1,5 +1,4 @@
-import 'dart:ffi';
-
+import 'package:flame/flame.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,20 +7,26 @@ import 'package:starshipasteroid/gameController.dart';
 class Jugador{
   final GameController gameController;
   
+  Sprite nave;
   double vidaMaxima;
   double vidaActual;
   Rect jugadorRect;
   bool muerto=false;
 
   Jugador(this.gameController){
+    Flame.images.loadAll(<String>[
+      'nave.png',
+    ]);
     vidaMaxima=vidaActual=300;
     final size =gameController.tileSize*1.5;
     jugadorRect=Rect.fromLTWH(gameController.screenSize.width/2-size/2, gameController.screenSize.height/2-size/2,size, size);
+    nave=Sprite('nave.png');
   }
  void render(Canvas canvas){
-    Paint fondoPaint=Paint()..color=Colors.amber;
-    
+    Paint fondoPaint=Paint()..color=Colors.amber;    
     canvas.drawRect(jugadorRect, fondoPaint);
+    final size =gameController.tileSize*1.5;
+    nave.renderRect(canvas, Rect.fromLTWH(gameController.screenSize.width/2-size/2, gameController.screenSize.height/2-size/2,size, size));
   }
   void update(double t) {
     if(!muerto&&vidaActual<=0){
